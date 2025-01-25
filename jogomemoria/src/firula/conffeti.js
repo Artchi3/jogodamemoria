7c7bfd04ddc321a81878fc351440ac2b560ab3a5
Artchi3 <arturrsilva.ars@gmail.com>
2025-01-25 14:28:25 -0300

    fix: adição da firula

---
+++ jogomemoria/src/firula/conffeti.js
@@ -0,0 +1,62 @@
+import confetti from "canvas-confetti";
+export const lounchSingle =()=>{
+const scalar = 2;
+const unicorn = confetti.shapeFromText({ text: '♥️', scalar });
+
+const defaults = {
+  spread: 360,
+  ticks: 60,
+  gravity: 0,
+  decay: 0.96,
+  startVelocity: 20,
+  shapes: [unicorn],
+  scalar,
+  colors: ['5E2129', 'FFFFFF', 'FF0000']
+};
+
+function shoot() {
+  confetti({
+    ...defaults,
+    particleCount: 30
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 5,
+    flat: true
+  });
+
+  confetti({
+    ...defaults,
+    particleCount: 15,
+    scalar: scalar / 2,
+    shapes: ['circle']
+  });
+}
+
+setTimeout(shoot, 0);
+setTimeout(shoot, 100);
+setTimeout(shoot, 200);
+}
+export const lounchMultiple =()=>{
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0,colors: ['5E2129', 'FFFFFF', 'FF0000','#35654D'] };
+    
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+    
+    const interval = setInterval(function() {
+      const timeLeft = animationEnd - Date.now();
+    
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+    
+      const particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+      confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+    }, 250);
+}
