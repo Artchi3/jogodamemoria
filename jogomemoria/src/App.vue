9c5de695fc5e4ecd9ff2e56ef465f70baa4b2b4e
Artchi3 <arturrsilva.ars@gmail.com>
2025-01-26 21:41:15 -0300

    fix: ajustes finos

--- jogomemoria/src/App.vue
+++ jogomemoria/src/App.vue
@@ -346,10 +346,10 @@ h1{
   top: 0;
   bottom: 0;
   left: 0;
-  right: 0;
-  margin: auto;
-  width: 100vw;
-  height: 100vh;
+  right: 0;    
+  margin: 0;
+    width: 100vw;
+    height: auto;
 }
 .comemoracao.is-active{
   visibility: visible;
@@ -513,10 +513,9 @@ button{
     bottom: 0;
     left: 0;
     right: 0;
-    margin: 0 !important;
+    margin: 0 !important; 
     width: 100vw;
-    height: 100vh;
-    min-height: 1020px;
+    height: auto;
   }
   .comemoracao.is-active{
     visibility: visible;
